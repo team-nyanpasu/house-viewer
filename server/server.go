c8b57d98c7dc05f40a7b3729c2be89ac63fbbd80
Ivan <29393345+hivanco@users.noreply.github.com>
2021-08-21 17:30:47 -0400

    Useless CRUD server

--- server/server.go
+++ server/server.go
@@ -6,29 +6,36 @@ import (
     "net/http"
 )
 
-type Bounds struct {
-    topLeft int
-    botRight int
-}
-
-func (b *Bounds) String() string {
-    return fmt.Sprintf("{%v, %v}", b.topLeft, b.botRight)
-}
-
-func parseBounds(boundsS string) (Bounds, error) {
-    return Bounds{0, 0}, nil
-}
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!\n", r.URL.Path[1:])
-    bounds, err := parseBounds(r.URL.Path[1:])
-    if err != nil {
-        fmt.Fprintf(w, "error\n")
-    }
-    fmt.Fprintf(w, "%v\n", bounds)
+    http.ServeFile(w, r, "server.go")
+}
+
+func createHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Create request from %s!\n", r.URL.Path)
+    fmt.Fprintf(w, "%+v", r)
+}
+
+func readHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Read request from %s!\n", r.URL.Path)
+    fmt.Fprintf(w, "%+v", r)
+}
+
+func updateHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Update request from %s!\n", r.URL.Path)
+    fmt.Fprintf(w, "%+v", r)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Delete request from %s!\n", r.URL.Path)
+    fmt.Fprintf(w, "%+v", r)
 }
 
 func main() {
-    fmt.Println("Starting house-viewer version 0.1")
+    fmt.Println("Starting issue tracker version 0.1")
     http.HandleFunc("/", handler)
+    http.HandleFunc("/create", createHandler)
+    http.HandleFunc("/read", readHandler)
+    http.HandleFunc("/update", updateHandler)
+    http.HandleFunc("/delete", deleteHandler)
     log.Fatal(http.ListenAndServe(":3000", nil))
 }
